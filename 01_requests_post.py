d9c7d9cea5b36d80188452a568e03b7cc76b26dc
SDSIQI <34374864+SIQILZ@users.noreply.github.com>
2018-01-07 22:32:17 +0800

    Update 01_requests_post.py

--- 01_requests_post.py
+++ 01_requests_post.py
@@ -5,10 +5,8 @@ headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64)'
                          'AppleWebKit/537.36 (KHTML, like Gecko)'
                          'Chrome/63.0.3239.108 Safari/537.36'}
 s = requests.session()
-
 data = {'username': 'XXXXXX', 'password': 'XXXXXX'}
 s.post(url,data=data, headers=headers)
-
 r = s.get('https://cn.tecdoc.net/_chooser/mfrs?year=&mfr=&model=&vehicle_type=&ctx=%2F&ltt=ktype&lang=zh')
 r.encoding = r.apparent_encoding
 str_json = json.loads(r.text)
